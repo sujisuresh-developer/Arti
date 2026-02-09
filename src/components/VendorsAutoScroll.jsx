95f4daab897551d157dc1af26298b3e55a1d8d69
sujisuresh-developer <sujisuresh313@gmail.com>
2026-02-09 11:42:33 +0530

    logos

--- src/components/VendorsAutoScroll.jsx
+++ src/components/VendorsAutoScroll.jsx
@@ -1,93 +1,154 @@
-"use client";
+// "use client";
 
 
+// const vendors = [
+//   { name: "Sophos", logo: "https://thewealthmosaic.s3.amazonaws.com/media/Logo_Sophos.png" },
+//   { name: "Fortinet", logo: "https://www.proofpoint.com/sites/default/files/pr/Proofpoint-logo-reg-K.jpg" },
+//   { name: "Check Point", logo: "https://www.eplus.com/images/default-source/authors/check-point-2024-logo-color.png?sfvrsn=69ac24b1_1" },
+//   { name: "Aruba", logo: "https://interworks.com/wp-content/uploads/2017/09/Aruba.png" },
+//   { name: "Veeam", logo: "https://securityaffairs.com/wp-content/uploads/2023/03/veeam-software-vector-logo.png" },
+//   { name: "Trend Micro", logo: "https://upload.wikimedia.org/wikipedia/en/7/7f/Trend_Micro_Logo_2023.png" },
+//   { name: "Mimecast", logo: "https://cybercompare.com/wp-content/uploads/2023/10/pd_mimecast_logo-1024x298.png" },
+//   { name: "Hewlett Packard", logo: "https://www.infosys.com/content/dam/infosys-web/en/about/images/hpe-logo.jpg" },
+//   { name: "VMware", logo: "https://generaltechnologies.co.in/assets/frontend/pages/img/brand_partner/vmware_1.png" },
+//   { name: "Dell Technologies", logo: "https://www.boardinfinity.com/blog/content/images/2025/02/Company-Blog-Creatives----2025-02-25T180208.318.png" },
+//   { name: "Microsoft", logo: "https://image.pitchbook.com/m3KpejK5fM5YbfW4TlFQw4MgQHh1707480684076_200x200" },
+//   { name: "Cisco", logo: "https://i0.wp.com/martinexsa.com/wp-content/uploads/2017/09/Cisco-logo.png?fit=271%2C190&ssl=1" },
+//   { name: "Palo Alto", logo: "https://www.paloaltonetworks.com/content/dam/pan/en_US/images/logos/brand/primary-company-logo/Parent-logo.png" },
+//   { name: "Nutanix", logo: "https://generaltechnologies.co.in/assets/frontend/pages/img/brand_partner/nutanix_logo.png" },
+// ];
+
+// const VendorsAutoScroll = () => {
+//   return (
+//     <>
+      
+
+//       <section className="bg-white py-28 overflow-hidden">
+//         <div className="max-w-[1600px] mx-auto">
+
+//           {/* OPTIONAL HEADING */}
+//           <div className="text-center mb-20 px-6">
+//             <h1 className="text-4xl md:text-5xl font-extrabold text-black tracking-tight">
+//               Technology Partners
+//             </h1>
+//             <p className="mt-4 text-gray-600">
+//               Trusted global vendors powering our solutions
+//             </p>
+//           </div>
+
+//           {/* AUTO SCROLL */}
+//           <div className="relative overflow-hidden">
+//             <div className="flex items-center gap-5 animate-vendorsLarge whitespace-nowrap px-12">
+
+//               {[...vendors, ...vendors].map((vendor, index) => (
+//                 <div
+//                   key={index}
+//                   className="flex items-center justify-center min-w-[300px]"
+//                 >
+//                   <img
+//                     src={vendor.logo}
+//                     alt={vendor.name}
+//                     className="
+//                       h-24
+//                       md:h-28
+//                       lg:h-32
+//                       xl:h-36
+//                       object-contain
+//                       grayscale
+//                       hover:grayscale-0
+//                       transition
+//                       duration-500
+//                     "
+//                   />
+//                 </div>
+//               ))}
+
+//             </div>
+//           </div>
+
+//         </div>
+//       </section>
+
+      
+
+//       {/* 🔹 SCROLL ANIMATION */}
+//       <style>{`
+//         @keyframes vendorsLarge {
+//           from {
+//             transform: translateX(0);
+//           }
+//           to {
+//             transform: translateX(-50%);
+//           }
+//         }
+
+//         .animate-vendorsLarge {
+//           animation: vendorsLarge 20s linear infinite;
+//         }
+//       `}</style>
+//     </>
+//   );
+// };
+
+// export default VendorsAutoScroll;
+import React from 'react'
 const vendors = [
-  { name: "Sophos", logo: "https://thewealthmosaic.s3.amazonaws.com/media/Logo_Sophos.png" },
+  { name: "Sophos", logo: "https://interworks.com/wp-content/uploads/2017/09/Sophos-ATC-LogoSQ.png",size: "large" },
   { name: "Fortinet", logo: "https://www.proofpoint.com/sites/default/files/pr/Proofpoint-logo-reg-K.jpg" },
-  { name: "Check Point", logo: "https://www.eplus.com/images/default-source/authors/check-point-2024-logo-color.png?sfvrsn=69ac24b1_1" },
-  { name: "Aruba", logo: "https://interworks.com/wp-content/uploads/2017/09/Aruba.png" },
+  { name: "Check Point", logo: "https://www.eplus.com/images/default-source/authors/check-point-2024-logo-color.png?sfvrsn=69ac24b1_1",size: "large" },
+  { name: "Aruba", logo: "https://interworks.com/wp-content/uploads/2017/09/Aruba.png" ,size: "large"},
   { name: "Veeam", logo: "https://securityaffairs.com/wp-content/uploads/2023/03/veeam-software-vector-logo.png" },
   { name: "Trend Micro", logo: "https://upload.wikimedia.org/wikipedia/en/7/7f/Trend_Micro_Logo_2023.png" },
+  { name: "Proofpoint", logo: "https://www.proofpoint.com/sites/default/files/pr/Proofpoint-logo-reg-K.jpg" },
   { name: "Mimecast", logo: "https://cybercompare.com/wp-content/uploads/2023/10/pd_mimecast_logo-1024x298.png" },
-  { name: "Hewlett Packard", logo: "https://www.infosys.com/content/dam/infosys-web/en/about/images/hpe-logo.jpg" },
-  { name: "VMware", logo: "https://generaltechnologies.co.in/assets/frontend/pages/img/brand_partner/vmware_1.png" },
-  { name: "Dell Technologies", logo: "https://www.boardinfinity.com/blog/content/images/2025/02/Company-Blog-Creatives----2025-02-25T180208.318.png" },
-  { name: "Microsoft", logo: "https://image.pitchbook.com/m3KpejK5fM5YbfW4TlFQw4MgQHh1707480684076_200x200" },
-  { name: "Cisco", logo: "https://i0.wp.com/martinexsa.com/wp-content/uploads/2017/09/Cisco-logo.png?fit=271%2C190&ssl=1" },
-  { name: "Palo Alto", logo: "https://www.paloaltonetworks.com/content/dam/pan/en_US/images/logos/brand/primary-company-logo/Parent-logo.png" },
-  { name: "Nutanix", logo: "https://generaltechnologies.co.in/assets/frontend/pages/img/brand_partner/nutanix_logo.png" },
+  { name: "Hewlett Packard", logo: "https://www.infosys.com/content/dam/infosys-web/en/about/images/hpe-logo.jpg",size: "large" },
+  { name: "VMware", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQizSkzySNMILzBu5JOtktZvfW7BuogHuAB7A&s" },
+  { name: "Dell Technologies", logo: "https://www.boardinfinity.com/blog/content/images/2025/02/Company-Blog-Creatives----2025-02-25T180208.318.png",size: "large" },
+  { name: "Microsoft", logo: "https://image.pitchbook.com/m3KpejK5fM5YbfW4TlFQw4MgQHh1707480684076_200x200",size: "large" },
+  { name: "Palo Alto Networks", logo: "https://www.paloaltonetworks.com/content/dam/pan/en_US/images/logos/brand/primary-company-logo/Parent-logo.png?imwidth=480",size: "large" },
+  { name: "Acronis", logo: "https://www.codelattice.com/assets/img/acronis/acronis-og.jpg" },
+  { name: "SonicWall", logo: "https://www.advantage.tech/wp-content/uploads/2023/07/Sonicwall-Brand-Logo.png",size: "large" },
+  { name: "3CX", logo: "https://uploads-ssl.webflow.com/5f9c23d3d16c8c72447ca60d/61bb5dfd7858b76a2d8e1faa_3CX%20Logo.jpg",size: "large" },
+  { name: "Huawei", logo: "https://media.business-humanrights.org/media/images/Huawei-Logo.2e16d0ba.fill-1200x630.png",size: "large" },
+  { name: "Cisco", logo: "https://i0.wp.com/martinexsa.com/wp-content/uploads/2017/09/Cisco-logo.png?fit=271%2C190&ssl=1",size: "large" },
+  { name: "Nutanix", logo: "https://generaltechnologies.co.in/assets/frontend/pages/img/brand_partner/nutanix_logo.png",size: "large" },
+  { name: "ESET", logo: "https://interworks.com/wp-content/uploads/2017/09/ESET-Logo_Web_Transparent.png",size: "large" },
+  { name: "Kaspersky", logo: "https://assets.dealmela.com/stores/kaspersky.png",size: "large" },
+  { name: "F5", logo: "https://www.bdata.com.hk/uploads/f5.png" },
+  { name: "Microsoft Azure", logo: "https://miro.medium.com/v2/resize:fit:1200/1*nzsbJWmPOvKI9yeBGHe2uA.png",size: "large" },
+  { name: "Ivanti", logo: "https://cdn-1.webcatalog.io/catalog/ivanti/ivanti-social-preview.png?v=1727416919549",size: "large" },
 ];
-
-const VendorsAutoScroll = () => {
-  return (
-    <>
-      
-
-      <section className="bg-white py-28 overflow-hidden">
-        <div className="max-w-[1600px] mx-auto">
-
-          {/* OPTIONAL HEADING */}
-          <div className="text-center mb-20 px-6">
+const VendorsAutoScroll = () => (
+  <section className="w-full bg-white py-8 overflow-hidden">
+     <div className="text-center mb-20 px-6">
             <h1 className="text-4xl md:text-5xl font-extrabold text-black tracking-tight">
-              Technology Partners
-            </h1>
-            <p className="mt-4 text-gray-600">
-              Trusted global vendors powering our solutions
-            </p>
+            Technology Partners           </h1>
+         <p className="mt-4 text-gray-600">
+                      Trusted global vendors powering our solutions
+             </p>
           </div>
+    <div className="flex items-center gap-14 whitespace-nowrap animate-vendors px-6 w-max">
+      {[...vendors, ...vendors].map((v, i) => (
+        <img
+  key={i}
+  src={v.logo}
+  alt={v.name}
+ className={`
+  object-contain opacity-90
+  ${
+    v.name === "Sophos" || v.name === "Aruba" ||v.name==="Microsoft"||v.name==="Ivanti"
+      ? "h-26 sm:h-24 xl:h-28"
+      : v.size === "large"
+      ? "h-20 sm:h-20 xl:h-24"
+      : "h-10 sm:h-12"
+  }
+`}
 
-          {/* AUTO SCROLL */}
-          <div className="relative overflow-hidden">
-            <div className="flex items-center gap-5 animate-vendorsLarge whitespace-nowrap px-12">
+/>
 
-              {[...vendors, ...vendors].map((vendor, index) => (
-                <div
-                  key={index}
-                  className="flex items-center justify-center min-w-[300px]"
-                >
-                  <img
-                    src={vendor.logo}
-                    alt={vendor.name}
-                    className="
-                      h-24
-                      md:h-28
-                      lg:h-32
-                      xl:h-36
-                      object-contain
-                      grayscale
-                      hover:grayscale-0
-                      transition
-                      duration-500
-                    "
-                  />
-                </div>
-              ))}
-
-            </div>
-          </div>
-
-        </div>
-      </section>
-
-      
-
-      {/* 🔹 SCROLL ANIMATION */}
-      <style>{`
-        @keyframes vendorsLarge {
-          from {
-            transform: translateX(0);
-          }
-          to {
-            transform: translateX(-50%);
-          }
-        }
-
-        .animate-vendorsLarge {
-          animation: vendorsLarge 20s linear infinite;
-        }
-      `}</style>
-    </>
-  );
-};
-
-export default VendorsAutoScroll;
+      ))}
+    </div>
+  </section>
+ 
+)
+ export default VendorsAutoScroll;
